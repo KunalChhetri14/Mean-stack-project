a212b474862626c92d36c05f8fd78f1d0a9acaab
kunalchhetri14 <kunalchhetri14@gmail.com>
2019-10-13 18:05:54 +0530

    all commit

--- assignments/assignment2/assignment2Js.js
+++ assignments/assignment2/assignment2Js.js
@@ -1,11 +1,13 @@
+ob={};
 function submitbutton()
 {
-    ob={};
+    
     ob['name']=document.getElementById('name').value;
     ob['email']=document.getElementById('email').value;
     ob['place']=document.getElementById('place').value;
     ob['mobNo']=document.getElementById('mobNo').value;
     //alert("Name is "+ ob['name'])+ " email "+ob['email']+ " place is " + ob['place']+ " Mobile NO"+ob[mobNo];
+    console.log("inside submit");
     console.log(ob);
     //console.log("the "+pname);  not working
 }
@@ -14,8 +16,8 @@ function submitbutton()
 
 function disable()
 {
-  //document.getElementById('name').style.pointer-events:none;  
+  document.getElementById('name').style.display="none";  
   document.getElementById('idDisable').style.backgroundColor="green";
-  document.body.style.color="gray";
+  
    //alert("set");
 }
