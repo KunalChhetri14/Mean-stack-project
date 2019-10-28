df0a471bb7d241680fd81c2f776f694bb46aaf70
kunalchhetri14 <kunalchhetri14@gmail.com>
2019-10-28 18:17:57 +0530

    Live Session Practise

--- liveSession/liveSession5Angular/FirstAnglar/src/app/app.module.ts
+++ liveSession/liveSession5Angular/FirstAnglar/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NewComponentComponent } from './new-component/new-component.component';
@@ -10,7 +11,8 @@ import { NewComponentComponent } from './new-component/new-component.component';
     NewComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- liveSession/liveSession5Angular/FirstAnglar/src/app/new-component/new-component.component.ts
+++ liveSession/liveSession5Angular/FirstAnglar/src/app/new-component/new-component.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input } from '@angular/core';
 
 @Component({
   selector: 'app-new-component',
-  templateUrl: './new-component.component.html',
+  template: `<h1>Child component  From Parent is : {{name}}</h1>   `//'./new-component.component.html'
+  ,
   styleUrls: ['./new-component.component.css']
 })
 export class NewComponentComponent implements OnInit {
 
   constructor() { }
 
+  @Input() public parentData;
   ngOnInit() {
   }
   Name="kunal";
+  Email=""
 }
